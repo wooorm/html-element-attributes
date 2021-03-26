abedc0896b7a58207fe327c631355b81f6ee8f13
Titus Wormer <tituswormer@gmail.com>
2021-03-26 14:26:30 +0100

    Regenerate
    * Remove crawling from HTML 4
      The living standard is now the one and only place where work happens
    * Fix to remove global attributes from certain tag names
    * Add `height`, `width` to `<source>` (applicable when in `<picture>`)

--- build.js
+++ build.js
@@ -8,7 +8,6 @@ var unified = require('unified')
 var html = require('rehype-parse')
 var q = require('hast-util-select')
 var toString = require('hast-util-to-string')
-var ev = require('hast-util-is-event-handler')
 var map = require('.')
 
 var processor = unified().use(html)
@@ -21,62 +20,8 @@ if (!globals) {
   map['*'] = globals
 }
 
-var counter = 0
-var expect = 2
-
-// Crawl HTML 4.
-https.get('https://www.w3.org/TR/html4/index/attributes.html', onhtml4)
-
 // Crawl WHATWG HTML.
 https.get('https://html.spec.whatwg.org/multipage/indices.html', onhtml)
-
-function onhtml4(response) {
-  response.pipe(concat(onconcat)).on('error', bail)
-
-  function onconcat(buf) {
-    var nodes = q.selectAll('table tr', processor.parse(buf))
-    var index = -1
-    var offset
-    var name
-    var elements
-    var tagName
-    var attributes
-
-    // Throw if we didn’t match, e.g., when the spec updates.
-    if (nodes.length === 0) {
-      throw new Error('Missing results in html4')
-    }
-
-    while (++index < nodes.length) {
-      name = q.select('[title="Name"]', nodes[index])
-      elements = q.select('[title="Related Elements"]', nodes[index])
-
-      if (!name || !elements) {
-        continue
-      }
-
-      name = toString(name).trim()
-      elements = toString(elements)
-
-      if (!name || ev(name)) {
-        continue
-      }
-
-      elements = /All elements/.test(elements) ? ['*'] : elements.split(/,/g)
-      offset = -1
-      while (++offset < elements.length) {
-        tagName = elements[offset].toLowerCase().trim()
-        attributes = map[tagName] || (map[tagName] = [])
-
-        if (!attributes.includes(name)) {
-          attributes.push(name)
-        }
-      }
-    }
-
-    done()
-  }
-}
 
 function onhtml(response) {
   response.pipe(concat(onconcat)).on('error', bail)
@@ -84,6 +29,9 @@ function onhtml(response) {
   function onconcat(buf) {
     var nodes = q.selectAll('#attributes-1 tbody tr', processor.parse(buf))
     var index = -1
+    var result = {}
+    var keys
+    var key
     var name
     var elements
     var tagName
@@ -106,7 +54,7 @@ function onhtml(response) {
       offset = -1
       elements = /HTML elements/.test(elements)
         ? ['*']
-        : elements.split(/;/g).map((d) => d.trim())
+        : elements.split(/;/g).map((d) => d.replace(/\([^)]+\)/g, '').trim())
 
       while (++offset < elements.length) {
         tagName = elements[offset].toLowerCase().trim()
@@ -118,40 +66,24 @@ function onhtml(response) {
       }
     }
 
-    done()
-  }
-}
+    keys = Object.keys(map).sort()
+    index = -1
 
-// Generate the map.
-function done() {
-  counter++
+    while (++index < keys.length) {
+      key = keys[index]
+      map[key].sort()
 
-  if (counter !== expect) {
-    return
-  }
+      if (key !== '*') {
+        map[key] = map[key].filter(function (attribute) {
+          return !globals.includes(attribute)
+        })
+      }
 
-  var result = {}
-  var keys = Object.keys(map).sort()
-  var index = -1
-  var key
-
-  while (++index < keys.length) {
-    key = keys[index]
-    result[key] = map[key]
-    map[key] = map[key].sort()
-
-    if (key === '*') {
-      continue
+      if (map[key].length > 0) {
+        result[key] = map[key]
+      }
     }
 
-    map[key] = map[key].filter(function (attribute) {
-      return !globals.includes(attribute)
-    })
-
-    if (map[key].length === 0) {
-      delete map[key]
-    }
+    fs.writeFile('index.json', JSON.stringify(result, null, 2) + '\n', bail)
   }
-
-  fs.writeFile('index.json', JSON.stringify(result, 0, 2) + '\n', bail)
 }
